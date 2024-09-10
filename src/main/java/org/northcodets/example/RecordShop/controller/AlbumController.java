9d92c11f402339ff4820fdcf481afce3c74d648d
JannelJ <janneljones26@gmail.com>
2024-09-10 11:28:08 +0100

    feat: save changes

--- src/main/java/org/northcodets/example/RecordShop/controller/AlbumController.java
+++ src/main/java/org/northcodets/example/RecordShop/controller/AlbumController.java
@@ -4,6 +4,7 @@ package org.northcodets.example.RecordShop.controller;
 import org.northcodets.example.RecordShop.model.Album;
 import org.northcodets.example.RecordShop.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
